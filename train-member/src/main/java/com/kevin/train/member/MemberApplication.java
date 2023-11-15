57f791d7e2b0545cba81e8e299202f9007f6c803
kevin7gif <2632176642@qq.com>
2023-11-15 22:25:11 +0800

    修改member模块启动日志

--- train-member/src/main/java/com/kevin/train/member/MemberApplication.java
+++ train-member/src/main/java/com/kevin/train/member/MemberApplication.java
@@ -1,7 +1,10 @@
 package com.kevin.train.member;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 /**
  * @author kevin
@@ -10,7 +13,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class MemberApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment environment = app.run(args).getEnvironment();
+        LOG.info("启动成功！");
+        LOG.info("地址：\thttp://127.0.0.1:{}",environment.getProperty("server.port"));
     }
 }
