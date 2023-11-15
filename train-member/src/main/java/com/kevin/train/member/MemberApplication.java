26b8df6f86850022db41d027443e048c06dd781c
kevin7gif <2632176642@qq.com>
2023-11-15 17:13:57 +0800

    新增member会员模块

--- train-member/src/main/java/com/kevin/train/member/MemberApplication.java
+++ train-member/src/main/java/com/kevin/train/member/MemberApplication.java
@@ -1,15 +1,16 @@
-package com.kevin.train;
+package com.kevin.train.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
+/**
+ * @author kevin
+ * @version 1.0
+ * @date 2023-11-15 17:02
+ */
 @SpringBootApplication
-public class TrainApplication {
-
+public class MemberApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TrainApplication.class, args);
+        SpringApplication.run(MemberApplication.class);
     }
-
 }
